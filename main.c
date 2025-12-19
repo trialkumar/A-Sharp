e09066a09e0dfa3de2f48567a2946fa36bcb2892
aadesh006 <aadeshchaudhari14@gmail.com>
2025-12-19 19:42:53 +0000

    mades changes in files incorrespomdign to the vm files

--- main.c
+++ main.c
@@ -1,8 +1,11 @@
 #include "common.h"
 #include "chunk.h"
 #include "debug.h"
+#include "vm.h"
 
 int main(int argc, const char* argv[]){
+
+  initVM();
     Chunk chunk;
     initChunk(&chunk);
 
--- vm.c
+++ vm.c
@@ -18,7 +18,7 @@ void push(Value value){
     vm.stackTop++; //Move the pointer up
 }//Not checking for overflow yet!
 
-void pop(){
+Value pop(){
     vm.stackTop--;
     return *vm.stackTop;
 }
@@ -31,6 +31,39 @@ static InterpretResult run(){
     #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
 
     for(;;){
-        
+        #ifndef DEBUG_TRACE_EXECUTION
+        printf("        ");
+        for(Value* slot = vm.stack; slot < vm.stackTop; slot++){
+            printf("[ ");
+            printfValue(*slot);
+            printf(" ]");
+        }
+        printf("\n");
+        // Disassemble the current instruction so we see what's happening
+    disassembleInstruction(vm.chunk, (int)(vm.ip - vm.chunk->code));
+#endif
+
+    uint8_t instruction = READ_BYTE();
+    switch (instruction) {
+      case OP_CONSTANT: {
+        Value constant = READ_CONSTANT();
+        push(constant); // Push the number onto the stack
+        break;
+      }
+      case OP_RETURN: {
+        printValue(pop()); // Pop the result and print it
+        printf("\n");
+        return INTERPRET_OK;
+      }
     }
+  }
+
+#undef READ_BYTE
+#undef READ_CONSTANT
+}
+
+InterpretResult interpret(Chunk* chunk) {
+  vm.chunk = chunk;
+  vm.ip = vm.chunk->code; // Start IP at the beginning of the chunk
+  return run();
 }
