52fe559320772fa3dfcdc46aff77bd9a138d15a7
aadesh006 <aadeshchaudhari14@gmail.com>
2025-12-25 00:40:44 +0530

    Created: - static bool isFalsey - static bool valuesEqual
    Updated:
    - define BINARY_OP

--- vm.c
+++ vm.c
@@ -27,35 +27,45 @@ Value pop(){
     return *vm.stackTop;
 }
 
+static bool isFalsey(Value value) {
+  return IS_NIL(value) || (IS_BOOL(value) && !AS_BOOL(value));
+}
+
+static bool valuesEqual(Value a, Value b) {
+  if (a.type != b.type) return false;
+  switch (a.type) {
+    case VAL_BOOL:   return AS_BOOL(a) == AS_BOOL(b);
+    case VAL_NIL:    return true;
+    case VAL_NUMBER: return AS_NUMBER(a) == AS_NUMBER(b);
+    default:         return false; // Should not happen
+  }
+}
+
 //The Engine Loop
-static InterpretResult run(){
-    //macro: Read the byte currently pointed at by ip, then advance ip
-    #define READ_BYTE() (*vm.ip++)
-    //macro: Read the next, use it as an index and look up the constant
-    #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+static InterpretResult run() {
+#define READ_BYTE() (*vm.ip++)
+#define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
 
-    //macro: BINARY_OP
-    #define BINARY_OP(op) \
-        do { \
-          if(!IS_NUMBER(peek(0)) || !IS_NUMBER(peek(1))){ \
-            /*runtime error*/ \
-            return INTERPRET_RUNTIME_ERROR; \
-          } \
-          double b = AS_NUMBER(pop()); \
-          double a = AS_NUMBER(pop()); \
-          push (NUMBER_VAL(a op b)); \
-        }while(false)
+// New Macro that takes 2 arguments: The Result Type and the Operator
+#define BINARY_OP(valueType, op) \
+    do { \
+      if (!IS_NUMBER(peek(0)) || !IS_NUMBER(peek(1))) { \
+        return INTERPRET_RUNTIME_ERROR; \
+      } \
+      double b = AS_NUMBER(pop()); \
+      double a = AS_NUMBER(pop()); \
+      push(valueType(a op b)); \
+    } while (false)
 
-    for(;;){
-        #ifndef DEBUG_TRACE_EXECUTION
-        printf("        ");
-        for(Value* slot = vm.stack; slot < vm.stackTop; slot++){
-            printf("[ ");
-            printfValue(*slot);
-            printf(" ]");
-        }
-        printf("\n");
-        // Disassemble the current instruction so we see what's happening
+  for (;;) {
+#ifdef DEBUG_TRACE_EXECUTION
+    printf("          ");
+    for (Value* slot = vm.stack; slot < vm.stackTop; slot++) {
+      printf("[ ");
+      printValue(*slot);
+      printf(" ]");
+    }
+    printf("\n");
     disassembleInstruction(vm.chunk, (int)(vm.ip - vm.chunk->code));
 #endif
 
@@ -63,21 +73,34 @@ static InterpretResult run(){
     switch (instruction) {
       case OP_CONSTANT: {
         Value constant = READ_CONSTANT();
-        push(constant); // Push the number onto the stack
+        push(constant);
         break;
       }
-
-      case OP_NIL: push(NIL_VAL); break;
-      case OP_TRUE: push(BOOL_VAL(true)); break;
+      case OP_NIL:   push(NIL_VAL); break;
+      case OP_TRUE:  push(BOOL_VAL(true)); break;
       case OP_FALSE: push(BOOL_VAL(false)); break;
-
-      case OP_ADD:      BINARY_OP(+); break;
-      case OP_SUBTRACT: BINARY_OP(-); break;
-      case OP_MULTIPLY: BINARY_OP(*); break;
-      case OP_DIVIDE:   BINARY_OP(/); break;
       
+      case OP_EQUAL: {
+        Value b = pop();
+        Value a = pop();
+        push(BOOL_VAL(valuesEqual(a, b)));
+        break;
+      }
+      
+      case OP_GREATER:  BINARY_OP(BOOL_VAL, >); break;
+      case OP_LESS:     BINARY_OP(BOOL_VAL, <); break;
+      
+      case OP_ADD:      BINARY_OP(NUMBER_VAL, +); break;
+      case OP_SUBTRACT: BINARY_OP(NUMBER_VAL, -); break;
+      case OP_MULTIPLY: BINARY_OP(NUMBER_VAL, *); break;
+      case OP_DIVIDE:   BINARY_OP(NUMBER_VAL, /); break;
+      
+      case OP_NOT:
+        push(BOOL_VAL(isFalsey(pop())));
+        break;
+
       case OP_NEGATE:   
-        if (!IS_NUMBER(peek(0))){
+        if (!IS_NUMBER(peek(0))) {
           return INTERPRET_RUNTIME_ERROR;
         }
         push(NUMBER_VAL(-AS_NUMBER(pop())));
@@ -93,7 +116,7 @@ static InterpretResult run(){
 
 #undef READ_BYTE
 #undef READ_CONSTANT
-#undef BINARY_OP
+#undef BINARY_OP 
 }
 
 InterpretResult interpret(Chunk* chunk) {
