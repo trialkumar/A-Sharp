195fcec93e05446b8e4eb26ab8ee6e53c7d4c677
aadesh006 <aadeshchaudhari14@gmail.com>
2026-01-07 19:33:12 +0530

    Add OP_LOOP support in the virtual machine and disassembler

--- vm.c
+++ vm.c
@@ -231,6 +231,13 @@ static InterpretResult run() {
         printf("\n");
         break;
       }
+
+      case OP_LOOP: {
+        uint16_t offset = READ_SHORT();
+        vm.ip -= offset;
+        break;
+      }
+
       case OP_RETURN: {
         // No longer prints result automatically
         return INTERPRET_OK;
